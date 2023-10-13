24a525a039dacc504c5bc5891b391c7a4054bd20
Zoren Mabunga <114373459+sirZz15@users.noreply.github.com>
2023-10-13 09:47:36 +0800

    Delete json_parser.py

--- json_parser.py
+++
@@ -1,7 +0,0 @@
-class JSONParser:
-    def __init__(self, data):
-        self.data = data
-
-    def extract_info(self):
-        # No need to parse 'self.data' as it's already a dictionary.
-        return self.data  # or process 'self.data' as needed
